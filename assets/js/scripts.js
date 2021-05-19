12573b0f82ce36f0736a6f9fa9553f3b1c4e63fa
maya <maya.arguell@gmail.com>
2021-05-19 18:22:39 -0400

    feature: dining and team templates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -82,6 +82,36 @@ $(document).ready(function(){
       }
     });
   }
+
+  if ( $('.team-list__item.full').length ) {
+    $('.team-list__item.full').each(function(){
+      const thisRef = this;
+      renderTeamBlockContent(this);
+      $(window).on('resize', function(){
+        renderTeamBlockContent(thisRef);
+      });
+    });
+  }
+
+  if ( $('.page-header__figure').length ) {
+    if ( $(window).scrollTop() > 0 ) {
+      $('.page-header__figure').css('transition', 'none');
+      $('.page-header__figure').css('opacity', 0);
+      $('.page-header__figure').addClass('scrolled');
+      setTimeout(function(){
+        $('.page-header__figure').css('transition', '');
+        $('.page-header__figure').css('opacity', '');
+      }, 100);
+    }
+
+    $(window).on('scroll', function(){
+      if ( $(window).scrollTop() > 0 ) {
+        $('.page-header__figure').addClass('scrolled');
+      } else {
+        $('.page-header__figure').removeClass('scrolled');
+      }
+    });
+  }
   /* END GENERIC SCRIPTS */
 
   /* Homepage */
@@ -110,16 +140,43 @@ $(document).ready(function(){
   }
   /* END HOMEPAGE SCRIPTS */
 
+  /* Dining Landing */
+  if ( $('.dining-landing').length ) {
+    $('.dining-landing__right__list').slick({
+      arrows: false,
+      swipe: false,
+      fade: true
+    });
+
+    $('.dining-landing__left__list-item__link').each(function(index){
+      $(this).on('mouseover', function(){
+        if ( !$(this).hasClass('active') ) {
+          $('.dining-landing__left__list-item__link').removeClass('active');
+          $(this).addClass('active');
+          $('.dining-landing__right__list').slick('slickGoTo', index);
+        }
+      });
+    });
+  }
+  /* END DINING SCRIPTS */
+
   /* Gallery */
   if ( $('.bblock-gallery').length ) {
     $('.bblock-gallery').each(function(){
       const list = $(this).find('.bblock-gallery__list');
       const controls = $(this).find('.bblock-gallery__list__controls');
+      const page = $(this).find('.bblock-gallery__list__controls__page-current');
       list.slick({
         arrows: true,
         appendArrows: controls,
         swipe: false,
-        speed: 900
+        speed: 900,
+        prevArrow: '<button type="button" class="slick-prev bblock-gallery__list__button"><span class="screen-reader-text">Previous</span></button>',
+        nextArrow: '<button type="button" class="slick-next bblock-gallery__list__button"><span class="screen-reader-text">Next</span></button>'
+      });
+
+      list.on('beforeChange',  function(event, slick, currentSlide, nextSlide){
+        page.text(nextSlide + 1);
       });
     });
   }
@@ -135,3 +192,36 @@ $(window).resize(function(){
   isMobile = ( windowWidth <= mobileBreakpoint );
 
 }); // end window resize functions
+
+/*
+ * Functions
+ ***************************/
+ // team block content hider
+ function renderTeamBlockContent(block) {
+   if ( !isMobile ) {
+     $(block).removeClass('has-toggle');
+     const info = $(block).find('.team-list__item__info');
+     const figure = $(block).find('.team-list__item__figure');
+
+     if ( info.height() > figure.height() ) {
+       const oldheight = info.height();
+       $(block).addClass('has-toggle');
+       info.css('height', figure.height() + 'px');
+       info.html(info.html() + '<div class="team-list__item__expander__container"><button class="team-list__item__expander">Click to expand</button></div>');
+       info.find('.team-list__item__expander').on('click', function(){
+         info.find('.team-list__item__expander__container').fadeOut(400);
+         info.animate({
+           'height': oldheight
+         }, 600, function(){
+           $(block).removeClass('has-toggle');
+           info.find('.team-list__item__expander__container').remove();
+         });
+       }).on('keypress', handleKeypressADA);
+     }
+   } else {
+     $(block).removeClass('has-toggle');
+     if ( $(block).find('.team-list__item__expander__container').length ) {
+       $(block).find('.team-list__item__expander__container').remove();
+     }
+   }
+ }
